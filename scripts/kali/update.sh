225be05bb8b62ffc1aa01f27b804ce2e5a36cb65
Aun Raza <aun.raza@gmail.com>
2018-06-11 09:28:07 +0400

    Fixed issues with Linux header updates.

--- scripts/kali/update.sh
+++ scripts/kali/update.sh
@@ -4,6 +4,8 @@ arch="`uname -r | sed 's/^.*[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\(-[0-9]\{1,2\}
 
 apt-get update;
 
-apt-get -y upgrade linux-image-$arch;
-apt-get -y install linux-headers-`uname -r`;
-apt-get -y install build-essential
+new_arch=`apt-cache search linux-image | grep kali2 | head -1 | cut -f3,4,5 -d"-"`
+apt-get -y upgrade linux-image-$new_arch;
+apt-get -y install linux-headers-$new_arch;
+apt-get -y install build-essential;
+reboot
